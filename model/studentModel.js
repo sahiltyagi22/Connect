d3a2836de3485082c4537394a291147202c3d8cf
sahil <sahiltyagi1204@outlook.com>
2024-02-12 16:35:42 +0530

    student model

---
+++ model/studentModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const studentSchema = mongoose.Schema({
+    email : {
+        type:String,
+        required:true
+    },
+
+    password :{
+        type :String
+    },
+
+    name :{
+        type :String
+    }, 
+
+    rollno : {
+        type : String
+    },
+
+    school :{
+        type :String
+    },
+
+    year :{
+        type : Number
+    },
+
+    role:{
+        type:String,
+        default : 'student'
+    }
+})
+
+const studentModel = mongoose.model('studentModel' , studentSchema)
+
+module.exports = studentModel
