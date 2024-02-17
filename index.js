5a4853317911004a3d104cb31cb9391fd361314a
sahil <sahiltyagi1204@outlook.com>
2024-02-17 16:20:48 +0530

    middlewares added ejs

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const ejs = require('ejs')
+const path = require('path')
 const app = express()
 
 const alumniRoute = require('./routes/alumni')
 const meetingRoute = require('./routes/meeting')
 const authRoute = require('./routes/authRoute')
 const articleRoute = require('./routes/articles')
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 
 mongoose.connect('mongodb://127.0.0.1/alumni')
@@ -33,6 +39,10 @@ app.use(authRoute)
 app.use(meetingRoute)
 app.use(alumniRoute)
 app.use(articleRoute)
+
+app.all("*" , (req,res)=>{
+    res.send("404 not found")
+})
 
 
 app.listen(3000 ,()=>{
