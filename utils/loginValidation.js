ef1c3a72873de8424e8e0bf6412efd522d10a9cd
sahil <sahiltyagi1204@outlook.com>
2024-02-17 16:21:05 +0530

    registrations forms

---
+++ utils/loginValidation.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken')
+
+exports.loginValidation = function (req, res, next) {
+    const token = req.header('Authorization');
+
+    if (!token) {
+        return next(); // Continue to the next middleware if no token is provided
+    }else{
+        return res.redirect('/')
+    }
+
+    
+
+   next()
+};
